97087a58ad1ec81d6324f98e87f961d2ce1ff1a1
Rana_Medhat_Hussien <eng.ranamedhat2024@gmail.com>
2021-07-30 15:12:21 +0200

    first commit

---
+++ setbit .c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main (){
+     int num , bitnum ; 
+     printf ("enter the number= ");
+     scanf("%d",&num);
+     printf("Enter the bit number= ");
+     scanf("%d",&bitnum);
+	 num=(num|(1<<bitnum));
+	 printf("The result is %d",num);
+	return 0;	
+}
